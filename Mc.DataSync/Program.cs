480554f31dbd42b2b50304ee6c40c913d139a670
mc <119139463@qq.com>
2019-12-07 22:13:47 +0800

    support

--- Mc.DataSync/Program.cs
+++ Mc.DataSync/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 using MiniAbp;
+using MiniAbp.Runtime;
 
 namespace Mc.DataSync
 {
@@ -15,16 +19,59 @@ namespace Mc.DataSync
         [STAThread]
         static void Main()
         {
-            Initiliaze();
+            MiniAbp.MiniAbp.StartWithSqlServer(GetConnectionString());
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmDashboard());
         }
-
-        static readonly YBootstrapper Bootstrapper = new YBootstrapper();
-        private static void Initiliaze()
+        /// <summary>
+        /// 读取配置信息
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static T AppSettings<T>(string key)
         {
-            Bootstrapper.Initialize();
+            try
+            {
+                var config = ConfigurationManager.AppSettings[key];
+                var value = System.Convert.ChangeType(config, typeof(T));
+                return (T)value;
+            }
+            catch (Exception)
+            {
+                return default(T);
+            }
         }
+        private static string GetConnectionString()
+        {
+            if (AppSettings<bool>("IsDebug"))
+            {
+                var defaultConn = ConfigurationManager.ConnectionStrings[AppSettings<string>("ConnectionStringName")]?.ConnectionString;
+                return defaultConn;
+            }
+            else
+            {
+                DirectoryInfo di = new DirectoryInfo(AppPath.RootPath);
+                var rootPath = di.Parent.Parent.FullName + "\\";
+                var webConfig = Path.Combine(rootPath, "Web.Config");
+                //将XML文件加载进来
+                XDocument document = XDocument.Load(webConfig);
+                //获取到XML的根元素进行操作
+                XElement root = document.Root;
+                XElement ele = root.Element("connectionStrings");
+                var adds = ele.Elements("add");
+                foreach (var item in adds)
+                {
+                    if (item.Attribute("name").Value == AppSettings<string>("ConnectionStringName"))
+                    {
+                        return item.Attribute("connectionString").Value;
+                    }
+                }
+            }
+            MessageBox.Show("找不到链接字符串或链接字符串错误，请检查config>ConnectionStringName");
+            return null;
+        }
+
     }
 }
